a3c463e0a2cdd9b98f72037f564799f848ada57a
Alju <aljithmadambichonedan@gmail.com>
2023-10-18 11:14:39 +0530

    Update privacy policy

--- Privacy Policy.swift
+++ Privacy Policy.swift
@@ -1,2 +1,40 @@
-//- Privacy Policy
-// The App Modularize does not collect any data at all from the user 
+// - Privacy Policy
+//  The App Modularize does not collect any data at all from the user
+// Privacy Policy for Modularize
+//
+// Last updated: 18/10/2023
+//
+// Modularize is committed to protecting your privacy. This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information.
+//
+// Information We Do Not Collect:
+//
+// Modularize is an offline application and does not collect any personal or user data.
+//
+// We do not track your location or usage patterns.
+//
+// We do not store, transmit, or share any user information or task data.
+//
+// We do not use cookies or any other tracking mechanisms.
+//
+// We do not integrate with third-party services or collect data through third-party plugins.
+//
+// Data Storage and Security:
+//
+// Your task and project data are stored locally on your device.
+//
+// We do not have access to your data, and it remains under your control.
+//
+// We take user data privacy and security seriously, ensuring your tasks remain private and protected.
+//
+// Children's Privacy:
+//
+// Modularize is intended for general audiences and is not directed at children under 4 years of age.
+// Contact Us:
+//
+// If you have any questions or concerns about our privacy practices, please contact us at aljithchonedan999@gmail.com
+//
+// Changes to this Privacy Policy:
+//
+// We may update our Privacy Policy to reflect changes to our practices or for other operational, legal, or regulatory reasons. Any changes will be posted on this page with an updated date.
+//
+// By using Modularize, you agree to the terms outlined in this Privacy Policy.
